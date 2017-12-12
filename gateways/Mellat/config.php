e4f1f0958ae4e4ea2e1dae5922508a6ab774e3c2
Mohammadreza Poursoltan <mrp1111@gmail.com>
2017-12-12 12:42:59 +0330

    Update config.php

--- gateways/Mellat/config.php
+++ gateways/Mellat/config.php
@@ -1,13 +1,6 @@
 <?php
-
-$terminalId = "2862735" ; //Your Mellat Payment Terminal ID .
-$userName = "fanavazeh1" ; //Your Mellat Payment User Name .
-$userPassword = "43463796" ; //Your Mellat Payment User Password .
-
-$SystemURL = "https://mihanservice.com/clients"; //Your WHMCS Url .
-
-
-
-
-
+$terminalId = "aaa";
+$userName = "bbb";
+$userPassword = "ccc";
+$SystemURL = "WHMCS_URL";
 ?>
